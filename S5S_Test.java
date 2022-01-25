f94fc15243d3936a6e01399d6055705ba6b86f6e
J-45 <64985480+J-45@users.noreply.github.com>
2022-01-25 18:13:02 +0100

    Update S5S_Test.java

--- S5S_Test.java
+++ S5S_Test.java
@@ -2,6 +2,13 @@ package eu.j45.sock5;
 
 import java.net.*;
 import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 import java.io.*;
 
 public class S5S_Test {
@@ -9,65 +16,62 @@ public class S5S_Test {
  private Socket socket = null;
  private OutputStream out = null;
  private InputStream in = null;
-
+ private String address = "127.0.0.1";
+ private int port = 5000;
+ private ByteArrayOutputStream baos = null;
  // constructor to put ip address and port
- public S5S_Test(String address, int port) {
+
   // establish a connection
-  try {
-   socket = new Socket(address, port);
-   if (socket.isConnected()) {
-    System.out.println("Connected");
-   }
 
-   // sends output to the socket
-   out = new DataOutputStream(socket.getOutputStream());
-   //takes input from socket
-   in = new DataInputStream(socket.getInputStream());
-  } catch (UnknownHostException u) {
-   System.out.println(u);
-  } catch (IOException i) {
-   System.out.println(i);
+  @BeforeEach
+  void openSocket_Test() throws UnknownHostException, IOException {
+	  this.socket = new Socket(this.address, this.port);
+	   if (this.socket.isConnected()) {
+	    System.out.println("Connected");
+	   }
+
+	   // sends output to the socket
+	   this.out = new DataOutputStream(this.socket.getOutputStream());
+	   //takes input from socket
+	   this.in = new DataInputStream(this.socket.getInputStream());
   }
-
   
-  try {
-   byte[] clientGreeting = {(byte)0x05, (byte)0x01, (byte)0x02};
-   out.write(clientGreeting);
+  @Test
+  @DisplayName("greeting")
+  void greeting_Test() throws IOException {
+	   byte[] clientGreeting = {(byte)0x05, (byte)0x01, (byte)0x02};
+	   this.out.write(clientGreeting);
 
-   String readableQuery = Arrays.toString(clientGreeting);
-   //printing request to console
-   System.out.println("Sent to server : " + readableQuery);
+	   String readableQuery = Arrays.toString(clientGreeting);
+	   //printing request to console
+	   System.out.println("Sent to server : " + readableQuery);
 
-   // Receiving reply from server
-   ByteArrayOutputStream baos = new ByteArrayOutputStream();
-   byte buffer[] = new byte[3];
-   baos.write(buffer, 0 , in.read(buffer));
-   
-   byte result[] = baos.toByteArray();
+	   // Receiving reply from server
+	   this.baos = new ByteArrayOutputStream();
+	   byte buffer[] = new byte[3];
+	   this.baos.write(buffer, 0 , this.in.read(buffer));
+	   
+	   byte result[] = this.baos.toByteArray();
 
-   String res = Arrays.toString(result);
+	   String res = Arrays.toString(result);
 
-   // printing reply to console
-   System.out.println("Recieved from server : " + res);
-  } catch (IOException i) {
-   System.out.println(i);
+	   // printing reply to console
+	   System.out.println("Recieved from server : " + res);
+	   String goodResp = "[5, 2]";
+	   
+	   assertEquals(goodResp, res);
+
   }
-  // }
-
-  // close the connection
-  try {
-   // input.close();
-   in.close();
-   out.close();
-   socket.close();
-  } catch (IOException i) {
-   System.out.println(i);
+  
+  @AfterEach
+  void closeSocket_Test() throws IOException {
+	  // close the connection
+	  this.in.close();
+	   this.out.close();
+	   this.socket.close();
+	   System.out.println("Deconnected");
   }
- }
 
- public static void main(String args[]) {
-  new S5S_Test("127.0.0.1", 5000);
- }
 }
 
 
