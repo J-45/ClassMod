d059d0ad5a1841b4d9fb8d1a8f034786cd7b6c8f
J-45 <64985480+J-45@users.noreply.github.com>
2022-01-28 19:32:24 +0100

    Update S5S.java

--- S5S.java
+++ S5S.java
@@ -2,72 +2,107 @@
 
 package eu.j45.sock5;
 
-import java.net.*;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
-import java.io.*; 
 
-public class S5S 
-{ 
- //initialize socket and input stream 
- private Socket   socket = null; 
- private ServerSocket server = null; 
- private InputStream in  = null; 
- private OutputStream out = null;
- private ByteArrayOutputStream baos = new ByteArrayOutputStream();
- private int port = 5000;
- 
- // constructor with port 
- public S5S(int port) throws IOException 
- { 
-  // starts server and waits for a connection 
+public class S5S {
 
-   this.server = new ServerSocket(this.port); 
-   System.out.println("Server started"); 
-   System.out.println("Waiting for a client ..."); 
-   this.socket = server.accept(); 
-   System.out.println("Client accepted"); 
-   
-   // takes input from the client socket 
-   this.in = new DataInputStream(this.socket.getInputStream()); 
-   //writes on client socket
-   this.out = new DataOutputStream(this.socket.getOutputStream());
-   
-   while (greeting() != true) {
-	   // code block to be executed
+   public static void main(String args[]) throws IOException {
+
+       ServerSocket listener = null;
+
+       System.out.println("Server is waiting to accept user...");
+       int clientNumber = 0;
+
+       // Try to open a server socket on port 7777
+       // Note that we can't choose a port less than 1023 if we are not
+       // privileged users (root)
+
+       try {
+           listener = new ServerSocket(5000);
+       } catch (IOException e) {
+           System.out.println(e);
+           System.exit(1);
+       }
+
+       try {
+           while (true) {
+               // Accept client connection request
+               // Get new Socket at Server.
+
+               Socket socketOfServer = listener.accept();
+               new ServiceThread(socketOfServer, clientNumber++).start();
+           }
+       } finally {
+           listener.close();
+       }
+
    }
 
-   System.out.println("Closing connection"); 
-   // close connection 
-   this.socket.close(); 
-   this.in.close(); 
-   
- }
- 
- public boolean greeting() throws IOException {
-	 
-	   // Receiving data from client
+   private static void log(String message) {
+       System.out.println(message);
+   }
 
-	   byte buffer[] = new byte[3];
-	   this.baos.write(buffer, 0 , this.in.read(buffer));
-	   byte result[] = this.baos.toByteArray();
-	   
-	   String readableRespond = Arrays.toString(result);
-	   System.out.println("Recieved from client : "+readableRespond); 
-	   
-	   byte serverGreeting[] = new byte[]{5,2};
-	   
-	   //echoing back to client
-	   if (Arrays.equals(result, new byte[] {5,1,2}) ) {
-		   this.out.write(serverGreeting);
-		   return true;
-	   }else {
-		   this.out.write(result);
-		   return false;
-	   }
- }
+   private static class ServiceThread extends Thread {
 
- public static void main(String args[]) throws IOException 
- { 
-  new S5S(5000); 
- } 
+       private int clientNumber;
+       private Socket socketOfServer;
+
+       public ServiceThread(Socket socketOfServer, int clientNumber) {
+           this.clientNumber = clientNumber;
+           this.socketOfServer = socketOfServer;
+
+           // Log
+           log("New connection with client# " + this.clientNumber + " at " + socketOfServer);
+       }
+
+       @Override
+       public void run() {
+
+           try {
+
+               // Open input and output streams
+               DataInputStream is = new DataInputStream(socketOfServer.getInputStream());
+               DataOutputStream os = new DataOutputStream(socketOfServer.getOutputStream());
+
+               while (true) {
+                   // Read data to the server (sent from client).
+                   
+                   
+                   byte buffer[] = new byte[3];
+                   ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            	   baos.write(buffer, 0 , is.read(buffer));
+            	   byte result[] = baos.toByteArray();
+            	   
+            	   String readableRespond = Arrays.toString(result);
+            	   System.out.println("Recieved from client : "+readableRespond); 
+            	   
+            	   byte serverGreeting[] = new byte[]{5,2};
+            	   byte serverEndingCom[] = new byte[]{5,(byte)0xFF};
+            	   
+                   
+                   // Write to socket of Server
+
+                   if (Arrays.equals(result, new byte[] {5,1,2})) {
+                       os.write(serverGreeting);
+//                       os.flush();
+                       break;
+                   }else {
+                	   os.write(serverEndingCom);
+//                     os.flush();
+                	   break;
+                   }
+               }
+
+           } catch (IOException e) {
+               System.out.println(e);
+               e.printStackTrace();
+           }
+       }
+   }
 }
