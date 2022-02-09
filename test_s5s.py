41435f4b072f1ac7fd372827a43dbf492371faca
J-45 <64985480+J-45@users.noreply.github.com>
2022-02-09 19:43:09 +0100

    Update test_s5s.py

--- test_s5s.py
+++ test_s5s.py
@@ -4,16 +4,19 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 8080
-VER = b'\x05' # SOCKS version (0x05)
-NAUTH = b'\x01' # Number of authentication methods supported, uint8
-AUTH = b'\x02' # Authentication method = Username/password
-STATUS_SUCCESS = b'\x00' # 0x00 success, otherwise failure, connection must be closed
-CMD = b'\x01' # Zstablish a TCP/IP stream connection
-RSV = b'\x00'
+
+
+
+
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_handle:
     socket_handle.connect((HOST, PORT))
+    VER = b'\x05' # SOCKS version (0x05)
+    NAUTH = b'\x01' # Number of authentication methods supported, uint8
+    AUTH = b'\x02' # Authentication method = Username/password
     CLIENT_GREETING = VER + NAUTH + AUTH
+
     print(f"Client sent:\t{CLIENT_GREETING}")
     socket_handle.sendall(CLIENT_GREETING)
     server_response = socket_handle.recv(1024)
@@ -30,18 +33,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_handle:
     socket_handle.sendall(CLIENT_AUTHENTICATION_REQUEST)
     server_response = socket_handle.recv(1024)
     print(f"Client got:\t{server_response}")
+    STATUS_SUCCESS = b'\x00' # 0x00 success, otherwise failure, connection must be closed
     AssertThat(server_response).IsEqualTo(AUTH + STATUS_SUCCESS)
 
+    CMD = b'\x01' # Zstablish a TCP/IP stream connection
+    RSV = b'\x00' # reserved, must be 0x00
     TYPE = b'\x03' # Domain name
+    SERVER_TYPE = b'\x01'
     DOMAIN = 'j45.eu'.encode()
     DSTADDR = TYPE + struct.pack('B', len(DOMAIN)) + DOMAIN
     DSTPORT = struct.pack(">H", 80)
+
     CLIENT_CONNECTION_REQUEST = VER + CMD + RSV + DSTADDR + DSTPORT
     print(f"Client sent:\t{CLIENT_CONNECTION_REQUEST}")
     socket_handle.sendall(CLIENT_CONNECTION_REQUEST)
     server_response = socket_handle.recv(1024)
     print(f"Client got:\t{server_response}")
-    AssertThat(server_response).IsEqualTo(AUTH + STATUS_SUCCESS)
+
+    
+
 
 # AND
 
